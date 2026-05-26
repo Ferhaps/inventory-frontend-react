511d809fa52b8390281a18b16a1b3abdf73ded53
Ferhan Cherkez <fericherkez14052002@gmail.com>
2026-05-26 23:37:42 +0300

    adds css and theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,23 @@ import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './routes';
+import { createTheme, ThemeProvider } from '@mui/material';
 import "./index.css";
 
 const queryClient = new QueryClient();
 
+const theme = createTheme({
+	palette: {
+		primary: { main: '#31adff' },
+	},
+});
+
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<QueryClientProvider client={queryClient}>
-			<RouterProvider router={router} />
+			<ThemeProvider theme={theme}>
+				<RouterProvider router={router} />
+			</ThemeProvider>
 		</QueryClientProvider>
 	</StrictMode>,
 );
